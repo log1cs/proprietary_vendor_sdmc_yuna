454eb00d616b87909aba5fea7cae91254bc64f18
Tuan Anh <tuan73176@gmail.com>
2026-09-03 16:29:07 +0700

    oppen-user 14 UKG3.250826.001 20260708 release-keys
    Change-Id: Ic6ab42152ddab7c590959e4d816a073c5d1298ef

--- Android.mk
+++ Android.mk
@@ -6,6 +6,6 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),yuna)
 
-$(call add-radio-file-sha1-checked,radio/bootloader.img,c4d0f5bbb9c8bcecf2f28fa7a3d9c61605f92666)
+$(call add-radio-file-sha1-checked,radio/bootloader.img,827edea69a55b209014a1624db3ad4a67794afe7)
 
 endif
